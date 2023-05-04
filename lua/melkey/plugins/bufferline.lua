2077a6b58c633814c6db897789bb446430d5ec6f
Arnold Chand <creativenull@outlook.com>
2023-05-04 00:25:46 -0400

    feat: bufferline

---
+++ lua/melkey/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup({})
+		end,
+	},
+}
