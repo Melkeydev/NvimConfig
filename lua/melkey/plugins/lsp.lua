30bc491c57c17327fae2beacb298e942639736f6
Arnold Chand <creativenull@outlook.com>
2023-05-04 15:42:55 -0400

    feat: initial copilot setup

--- lua/melkey/plugins/lsp.lua
+++ lua/melkey/plugins/lsp.lua
@@ -337,5 +337,14 @@ return {
 				}),
 			},
 		})
+
+		-- For copilot
+		cmp.event:on("menu_opened", function()
+			vim.b.copilot_suggestion_hidden = true
+		end)
+
+		cmp.event:on("menu_closed", function()
+			vim.b.copilot_suggestion_hidden = false
+		end)
 	end,
 }
