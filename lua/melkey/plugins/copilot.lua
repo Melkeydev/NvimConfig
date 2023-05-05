7cc660466f82d7400352ddd7f5e5711cbf368cec
Amokstakov <amokstakov@gmail.com>
2023-05-04 22:15:14 -0700

    removing shit

--- lua/melkey/plugins/copilot.lua
+++ lua/melkey/plugins/copilot.lua
@@ -5,7 +5,7 @@ return {
 			require("copilot").setup({
 				panel = {
 					enabled = true,
-					auto_refresh = false,
+					auto_refresh = true,
 					keymap = {
 						jump_prev = "[[",
 						jump_next = "]]",
@@ -16,12 +16,12 @@ return {
 				},
 				suggestion = {
 					enabled = true,
-					auto_trigger = false,
+					auto_trigger = true,
 					debounce = 75,
 					keymap = {
-						accept = "<M-l>",
-						next = "<M-]>",
-						prev = "<M-[>",
+						accept = "<leader>;",
+						next = "<leader>l",
+						prev = "<leader>k",
 						dismiss = "<C-]>",
 					},
 				},
